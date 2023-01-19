2ee5158cf102e06d3f2e1a8dd99025340b89c0d1
celhakiza <hakizacel@gmail.com>
2023-01-19 18:38:33 +0200

    added code to create line graph that change by hovering

--- pyram_app.py
+++ pyram_app.py
@@ -7,8 +7,9 @@ import datetime
 import json
 import pandas as pd
 import plotly.graph_objs as go
-import mysql.connector
+
 df_pyramid=pd.read_excel(r'C:\Users\ENVY\Desktop\Data science\pop2.xlsx',skiprows=4)
+df_tot_population=pd.read_excel(r'C:\Users\ENVY\PycharmProjects\Pyramid\Pyramid_Census\Total population.xlsx')
 
 
 app=dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP],
@@ -23,7 +24,7 @@ app.layout=dbc.Container(
                 dbc.Col(
                     html.H2('Population Pyramid Projection')
 
-                ,width=4)
+                ,className="text-center text-primary mb-4 font-weight-bold",width=12)
             ]
         ),
 
@@ -31,62 +32,121 @@ app.layout=dbc.Container(
             [
                 dbc.Col(
                     [
-                        html.H6('Slide from left to right to get the population projection for each year'),
-                       dcc.Slider(min=2013,max=2032,step=1,
-                                  value=2013,
-                                  id='pyram',
-                                  marks=None,
-                                  tooltip={"placement": "bottom", "always_visible": True}
-                                  ),
+                        dcc.Slider(id='line-input'),
+                        dcc.Graph(id='line-trend',figure={})
+                    ]
+                ,width=5),
+                dbc.Col(
+                    [
                         dcc.Graph(id='graph-pyr',figure={})
                     ]
-                )
+                ,width=7)
             ]
         )
     ]
 )
 @app.callback(
-    Output('graph-pyr','figure'),
-    Input('pyram','value')
+    Output('line-trend','figure'),
+    Input('line-input','value')
+
 )
-
-def pyramid(slctd_year):
-    df_pyramid_1=df_pyramid[df_pyramid['year']==slctd_year]
-    y_age = df_pyramid_1['age_group']
-    x_Male = df_pyramid_1['Male']
-    x_Female = df_pyramid_1['Female'] * -1
-
-    # instantiate figure
-    fig = go.Figure()
-    fig.add_trace(go.Bar(
-        y=y_age,
-        x=x_Male,
-        name='Male',
-        orientation='h'
-    ))
-    fig.add_trace(go.Bar(
-        y=y_age,
-        x=x_Female,
-        name='Female',
-        orientation='h'
-    ))
-
-    fig.update_layout(
-        template='plotly_white',
-        title='Rwanda Census Population Pyramid Projection in {}'.format(slctd_year),
-        title_font_size=24,
-        barmode='relative',
-        bargap=0.0,
-        bargroupgap=0,
-        plot_bgcolor='white',
-        xaxis=dict(
-            tickvals=[-1000000,-800000, -600000, -400000,-200000,-100000, 0,100000,200000, 400000, 600000, 800000,1000000],
-            ticktext=['1M','800k', '600k', '400k', '200k','100k', 0,'100k', '200k', '400k', '600k', '800k','1M'],
-            title='Population in Thousands'
-        )
-    )
-
+def line(line_input):
+    fig=px.line(data_frame=df_tot_population,
+                x='Year',
+                y='Population Projection ',
+                markers=True)
+    fig.update_layout(xaxis_title='Year',
+                      yaxis_title='Total Population',
+                      title='Population across year',
+                      title_font_size=24,
+                      title_font_color='blue')
     return fig
+@app.callback(
+    Output('graph-pyr','figure'),
+    Input('line-trend',component_property='hoverData')
+)
+def pyramid(slctd_year):
+    if slctd_year is None:
+        df_pyramid_1=df_pyramid[df_pyramid['year']==2013]
+        y_age = df_pyramid_1['age_group']
+        x_Male = df_pyramid_1['Male']
+        x_Female = df_pyramid_1['Female'] * -1
+
+        # instantiate figure
+        fig = go.Figure()
+        fig.add_trace(go.Bar(
+            y=y_age,
+            x=x_Male,
+            name='Male',
+            orientation='h'
+        ))
+        fig.add_trace(go.Bar(
+            y=y_age,
+            x=x_Female,
+            name='Female',
+            orientation='h'
+        ))
+
+        fig.update_layout(
+            template='plotly_white',
+            title='Rwanda Population Pyramid in {}'.format(2013),
+            title_font_size=24,
+            barmode='relative',
+            bargap=0.0,
+            bargroupgap=0,
+            plot_bgcolor='white',
+            title_font_color='blue',
+            xaxis=dict(
+                tickvals=[-1000000,-800000, -600000, -400000,-200000,-100000, 0,100000,200000, 400000, 600000, 800000,1000000],
+                ticktext=['1M','800k', '600k', '400k', '200k','100k', 0,'100k', '200k', '400k', '600k', '800k','1M'],
+                title='Population in Thousands'
+            )
+        )
+
+        return fig
+
+    else:
+        hover_year=slctd_year['points'][0]['x']
+        df_pyramid_1 = df_pyramid[df_pyramid['year'] == hover_year]
+        y_age = df_pyramid_1['age_group']
+        x_Male = df_pyramid_1['Male']
+        x_Female = df_pyramid_1['Female'] * -1
+
+        # instantiate figure
+        fig = go.Figure()
+        fig.add_trace(go.Bar(
+            y=y_age,
+            x=x_Male,
+            name='Male',
+            orientation='h'
+        ))
+        fig.add_trace(go.Bar(
+            y=y_age,
+            x=x_Female,
+            name='Female',
+            orientation='h'
+        ))
+
+        fig.update_layout(
+            template='plotly_white',
+            title='Rwanda Population Pyramid in {}'.format(hover_year),
+            title_font_size=24,
+            barmode='relative',
+            bargap=0.0,
+            bargroupgap=0,
+            plot_bgcolor='white',
+            title_font_color='blue',
+            xaxis=dict(
+                tickvals=[-1000000, -800000, -600000, -400000, -200000, -100000, 0, 100000, 200000, 400000, 600000,
+                          800000, 1000000],
+                ticktext=['1M', '800k', '600k', '400k', '200k', '100k', 0, '100k', '200k', '400k', '600k', '800k',
+                          '1M'],
+                title='Population in Thousands'
+            )
+        )
+
+        return fig
+
 
 
 
@@ -98,8 +158,6 @@ def pyramid(slctd_year):
 server=app.server
 app.run_server(debug=True,port=3300)
 
+#print(df_tot_population.head())
 
 
-
-# df_pyramid_1=df_pyramid[df_pyramid['year']==2019]
-# print(df_pyramid_1)
